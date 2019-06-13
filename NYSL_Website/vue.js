82a957b6c76f10378f6d070f3e54a92cf5ff12b5
rodarg <rodarg87@gmail.com>
2019-06-13 20:38:11 -0300

    diagrama

---
+++ NYSL_Website/vue.js
@@ -0,0 +1,126 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    PartidosSep : [{
+      fecha: '9/01',
+      equipo1:'U1',
+      equipo2:'U4',
+      location: "AJ Katzenmaier",
+      hora:'9:30 a.m.'
+    },
+    {
+      fecha: '9/01',
+      equipo1:'U3',
+      equipo2:'U2',
+      location: "Greenbay",
+      hora:'1:00 p.m.'
+    },
+    {
+      fecha: '9/08',
+      equipo1:'U5',
+      equipo2:'U6',
+      location: "Howard A Yeager",
+      hora:'9:30 a.m.'
+    },
+    {
+      fecha: '9/08',
+      equipo1:'U6',
+      equipo2:'U1',
+      location: "Marjorie P Hart",
+      hora:'1:00 p.m.'
+    },
+    {
+      fecha: '9/15',
+      equipo1:'U2',
+      equipo2:'U4',
+      location: "North",
+      hora:'9:30 a.m.'
+    },
+    {
+      fecha: '9/15',
+      equipo1:'U3',
+      equipo2:'U5',
+      location: "AJ Katzenmaier",
+      hora:'1:00 p.m.'
+    },
+    {
+      fecha: '9/22',
+      equipo1:'U1',
+      equipo2:'U3',
+      location: "South",
+      hora:'9:30 a.m.'
+    },
+    {
+      fecha: '9/22',
+      equipo1:'U2',
+      equipo2:'U6',
+      location: "Howard A Yeager",
+      hora:'1:00 p.m.'
+    },
+    {
+      fecha: '9/29',
+      equipo1:'U4',
+      equipo2:'U5',
+      location: "South",
+      hora:'9:30 a.m.'
+    },
+  ],
+  PartidosOct : [{
+    fecha: '10/06',
+    equipo1:'U2',
+    equipo2:'U5',
+    location: "Marjorie P Hart",
+    hora:'9:30 a.m.'
+  },
+  {
+    fecha: '10/06',
+    equipo1:'U1',
+    equipo2:'U6',
+    location: "South",
+    hora:'1:00 p.m.'
+  },
+  {
+    fecha: '10/08',
+    equipo1:'U3',
+    equipo2:'U4',
+    location: "Howard A Yeager",
+    hora:'9:30 a.m.'
+  },
+  {
+    fecha: '10/08',
+    equipo1:'U5',
+    equipo2:'U1',
+    location: "Greenbay",
+    hora:'1:00 p.m.'
+  },
+  {
+    fecha: '10/20',
+    equipo1:'U6',
+    equipo2:'U3',
+    location: "North",
+    hora:'9:30 a.m.'
+  },
+  {
+    fecha: '10/20',
+    equipo1:'U2',
+    equipo2:'U4',
+    location: "Marjorie P Hart",
+    hora:'1:00 p.m.'
+  },
+  {
+    fecha: '10/27',
+    equipo1:'U3',
+    equipo2:'U1',
+    location: "AJ Katzenmaier",
+    hora:'9:30 a.m.'
+  },
+  {
+    fecha: '10/27',
+    equipo1:'U6',
+    equipo2:'U4',
+    location: "Howard A Yeager",
+    hora:'1:00 p.m.'
+  },
+],
+  }
+})
