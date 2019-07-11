ff9a25acee4c82cd7de35ffbb02c7bddf12640e9
rodarg <rodarg87@gmail.com>
2019-07-10 23:27:56 -0300

    animations

--- NYSL_Website/public/vue.js
+++ NYSL_Website/public/vue.js
@@ -10,10 +10,18 @@
     var viewportWidth = $(window).width();
     if (viewportWidth < 767) {
             $("#nav").addClass("fixed");
-            $("#espacio").addClass("espacio");
+            $("#espacio").addClass("espacio2");
+            $("#espacio1").addClass("espacio");
+            $("#espacio2").addClass("espacio");
+            $("#espacio3").addClass("espacio");
+            $("#espacio4").addClass("espacio");
     }else{
         $("#nav").removeClass("fixed");
-        $("#espacio").removeClass("espacio");
+        $("#espacio").removeClass("espacio2");
+        $("#espacio1").removeClass("espacio");
+        $("#espacio2").removeClass("espacio");
+        $("#espacio3").removeClass("espacio");
+        $("#espacio4").removeClass("espacio");
     }
 }
 
@@ -44,6 +52,10 @@ $('#about').hide();
 });
 
 $("#index1").click(function() {
+  $("#index1").addClass("active");
+  $("#contact1").removeClass("active");
+  $("#rules1").removeClass("active");
+  $("#about1").removeClass("active");
   $('#myImage').show(); 
   $('#tabla').hide(); 
 $('#contact').hide(); 
@@ -54,6 +66,10 @@ $('#about').hide();
   $("#boton2").show();
 });
 $("#contact1").click(function() {
+  $("#contact1").addClass("active");
+  $("#index1").removeClass("active");
+  $("#rules1").removeClass("active");
+  $("#about1").removeClass("active");
   $("#botonOn").hide();
   $("#boton2").hide();
   $('#myImage').hide(); 
@@ -64,6 +80,10 @@ $('#about').hide();
   $('#Select').hide();
 });
 $("#rules1").click(function() {
+  $("#rules1").addClass("active");
+  $("#index1").removeClass("active");
+  $("#contact1").removeClass("active");
+  $("#about1").removeClass("active");
   $("#botonOn").hide();
   $("#boton2").hide();
   $('#myImage').hide(); 
@@ -74,6 +94,10 @@ $('#about').hide();
   $('#Select').hide();
 });
 $("#about1").click(function() {
+  $("#about1").addClass("active");
+  $("#index1").removeClass("active");
+  $("#rules1").removeClass("active");
+  $("#contact1").removeClass("active");
   $("#botonOn").hide();
   $("#boton2").hide();
   $('#myImage').hide(); 
