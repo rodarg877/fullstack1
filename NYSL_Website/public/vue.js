d353bd3c726d9c0eb642f00964b66e8447026868
rodarg <rodarg87@gmail.com>
2019-07-18 21:23:40 -0300

    funciona todo como el ort

--- NYSL_Website/public/vue.js
+++ NYSL_Website/public/vue.js
@@ -1,177 +1,185 @@
 function porTamaño() {
-    var viewportWidth = $(window).width();
-    if (viewportWidth < 767) {
-            $("#nav").addClass("fixed");
-            $("#espacio").addClass("espacio2");
-            $("#espacio1").addClass("espacio");
-            $("#espacio2").addClass("espacio");
-            $("#espacio3").addClass("espacio");
-            $("#espacio4").addClass("espacio");
-    }else{
-        $("#nav").removeClass("fixed");
-        $("#espacio").removeClass("espacio2");
-        $("#espacio1").removeClass("espacio");
-        $("#espacio2").removeClass("espacio");
-        $("#espacio3").removeClass("espacio");
-        $("#espacio4").removeClass("espacio");
-    }
+  var viewportWidth = $(window).width();
+  if (viewportWidth < 767) {
+    $("#nav").addClass("fixed");
+    $("#espacio").addClass("espacio2");
+    $("#espacio1").addClass("espacio");
+    $("#espacio2").addClass("espacio");
+    $("#espacio3").addClass("espacio");
+    $("#espacio4").addClass("espacio");
+  } else {
+    $("#nav").removeClass("fixed");
+    $("#espacio").removeClass("espacio2");
+    $("#espacio1").removeClass("espacio");
+    $("#espacio2").removeClass("espacio");
+    $("#espacio3").removeClass("espacio");
+    $("#espacio4").removeClass("espacio");
+  }
 }
 
-    $(window).on('load , resize', porTamaño);
+$(window).on("load , resize", porTamaño);
 
+$(document).ready(function() {
+  $("#tabla").hide();
+  $("#contact").hide();
+  $("#rules").hide();
+  $("#about").hide();
+  $("#botonOn").click(function() {
+    $("#tabla").show();
+    $("#myImage").hide();
+    $("#Select").hide();
+    $("#contact").hide();
+    $("#rules").hide();
+    $("#about").hide();
+  });
 
-  $(document).ready(function() { 
-$('#tabla').hide(); 
-$('#contact').hide(); 
-$('#rules').hide();
-$('#about').hide();
-$("#botonOn").click(function() {
-  $('#tabla').show(); 
-  $('#myImage').hide(); 
-  $('#Select').hide();
-  $('#contact').hide(); 
-$('#rules').hide();
-$('#about').hide();
-});
+  $("#boton2").click(function() {
+    $("#myImage").show();
+    $("#tabla").hide();
+    $("#Select").show();
+    $("#contact").hide();
+    $("#rules").hide();
+    $("#about").hide();
+  });
 
-$("#boton2").click(function() {
-  $('#myImage').show(); 
-  $('#tabla').hide(); 
-  $('#Select').show();
-  $('#contact').hide(); 
-$('#rules').hide();
-$('#about').hide();
+  $("#index1").click(function() {
+    $("#index1").addClass("active");
+    $("#contact1").removeClass("active");
+    $("#rules1").removeClass("active");
+    $("#about1").removeClass("active");
+    $("#myImage").show();
+    $("#tabla").hide();
+    $("#contact").hide();
+    $("#rules").hide();
+    $("#about").hide();
+    $("#Select").show();
+    $("#botonOn").show();
+    $("#boton2").show();
+  });
+  $("#contact1").click(function() {
+    $("#contact1").addClass("active");
+    $("#index1").removeClass("active");
+    $("#rules1").removeClass("active");
+    $("#about1").removeClass("active");
+    $("#botonOn").hide();
+    $("#boton2").hide();
+    $("#myImage").hide();
+    $("#tabla").hide();
+    $("#contact").show();
+    $("#rules").hide();
+    $("#about").hide();
+    $("#Select").hide();
+  });
+  $("#rules1").click(function() {
+    $("#rules1").addClass("active");
+    $("#index1").removeClass("active");
+    $("#contact1").removeClass("active");
+    $("#about1").removeClass("active");
+    $("#botonOn").hide();
+    $("#boton2").hide();
+    $("#myImage").hide();
+    $("#tabla").hide();
+    $("#contact").hide();
+    $("#rules").show();
+    $("#about").hide();
+    $("#Select").hide();
+  });
+  $("#about1").click(function() {
+    $("#about1").addClass("active");
+    $("#index1").removeClass("active");
+    $("#rules1").removeClass("active");
+    $("#contact1").removeClass("active");
+    $("#botonOn").hide();
+    $("#boton2").hide();
+    $("#myImage").hide();
+    $("#tabla").hide();
+    $("#contact").hide();
+    $("#rules").hide();
+    $("#about").show();
+    $("#Select").hide();
+  });
 });
-
-$("#index1").click(function() {
-  $("#index1").addClass("active");
-  $("#contact1").removeClass("active");
-  $("#rules1").removeClass("active");
-  $("#about1").removeClass("active");
-  $('#myImage').show(); 
-  $('#tabla').hide(); 
-$('#contact').hide(); 
-$('#rules').hide();
-$('#about').hide(); 
-  $('#Select').show();
-  $("#botonOn").show();
-  $("#boton2").show();
-});
-$("#contact1").click(function() {
-  $("#contact1").addClass("active");
-  $("#index1").removeClass("active");
-  $("#rules1").removeClass("active");
-  $("#about1").removeClass("active");
-  $("#botonOn").hide();
-  $("#boton2").hide();
-  $('#myImage').hide(); 
-  $('#tabla').hide(); 
-$('#contact').show(); 
-$('#rules').hide();
-$('#about').hide(); 
-  $('#Select').hide();
-});
-$("#rules1").click(function() {
-  $("#rules1").addClass("active");
-  $("#index1").removeClass("active");
-  $("#contact1").removeClass("active");
-  $("#about1").removeClass("active");
-  $("#botonOn").hide();
-  $("#boton2").hide();
-  $('#myImage').hide(); 
-  $('#tabla').hide(); 
-$('#contact').hide(); 
-$('#rules').show();
-$('#about').hide(); 
-  $('#Select').hide();
-});
-$("#about1").click(function() {
-  $("#about1").addClass("active");
-  $("#index1").removeClass("active");
-  $("#rules1").removeClass("active");
-  $("#contact1").removeClass("active");
-  $("#botonOn").hide();
-  $("#boton2").hide();
-  $('#myImage').hide(); 
-  $('#tabla').hide(); 
-$('#contact').hide(); 
-$('#rules').hide();
-$('#about').show(); 
-  $('#Select').hide();
-});
-});
-  var app = new Vue({
-  el: '#app',
+var app = new Vue({
+  el: "#app",
   data: {
-    index:5,
-    window:"",
-    select:'all',
-    DatosFiltrados:[],
-    Teams:[],
-   Partidos :[],
-   db:db,
+    index: 0,
+    window: "",
+    select: "all",
+    DatosFiltrados: [],
+    Teams: [],
+    Partidos: [],
+    db: db
   },
   methods: {
-    mostrarIndex: function(index1){
-this.index=index1;
-    },
-    cargarDatosTeams: function(){
-      this.db.collection('Teams').get().then(( snapshot ) => {
-        snapshot.docs.forEach(doc =>{
-           var team;             
-           team = doc.data();
-           //agrego al array
-           this.Teams.push(team);
-           
+    cargarDatosTeams: function() {
+      this.db
+        .collection("Teams")
+        .get()
+        .then(snapshot => {
+          snapshot.docs.forEach(doc => {
+            var team;
+            team = doc.data();
+            //agrego al array
+            this.Teams.push(team);
           });
         });
-      },
-      cargarDatosPartido: function(){
-        this.db.collection('Partidos').get().then(( snapshot ) => {
-          snapshot.docs.forEach(doc =>{
-             var partido;             
-             partido = doc.data();
-             //agrego al array
-             this.Partidos .push(partido);
-             
-            });
+    },
+    cargarDatosPartido: function() {
+      this.db
+        .collection("Partidos")
+        .get()
+        .then(snapshot => {
+          snapshot.docs.forEach(doc => {
+            var partido;
+            partido = doc.data();
+            //agrego al array
+            this.Partidos.push(partido);
           });
-        },
+        });
     },
-    created(){
-      this.cargarDatosPartido();
-      this.cargarDatosTeams();
-    },
+    mostrarIndex: function(index1) {
+      this.index = index1;
+    }
+  },
+  created() {
+    this.cargarDatosPartido();
+    this.cargarDatosTeams();
+  },
   computed: {
-    datosFiltrados: function () {
+    datosFiltrados: function() {
       if (this.select != "all") {
-       return this.DatosFiltrados = this.Partidos.filter((dato) => (dato.equipo1 == this.select || dato.equipo2 == this.select))
+        return (this.DatosFiltrados = this.Partidos.filter(
+          dato => dato.equipo1 == this.select || dato.equipo2 == this.select
+        ));
       } else {
-      return this.DatosFiltrados = this.Partidos
+        return (this.DatosFiltrados = this.Partidos);
       }
     }
   }
-})
-var provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
 });
+var provider = new firebase.auth.GoogleAuthProvider();
+firebase
+  .auth()
+  .signInWithPopup(provider)
+  .then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  })
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
 
-    $("#menu-toggle").click(function (e) {
-      e.preventDefault();
-      $("#wrapper").toggleClass("toggled");
-    });
-  
+$("#menu-toggle").click(function(e) {
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
+});
