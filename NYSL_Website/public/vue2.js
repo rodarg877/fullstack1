3258eb07fa28e443f08a6ab3bcd9a274c62cbed7
rodarg <rodarg87@gmail.com>
2019-08-01 23:22:34 -0300

    de a poco

--- NYSL_Website/public/vue2.js
+++ NYSL_Website/public/vue2.js
@@ -27,7 +27,7 @@ function log(){
   var Pass= document.getElementById("Pass").value;
   firebase.auth().signInWithEmailAndPassword(Email,Pass).catch(function(error) {
     // Handle Errors here.
-    var errorCode= error.code;
+    var errorCode=  error.code;
     var errorMenssage= error.message;
     // ...
   });
@@ -77,7 +77,20 @@ function registroGoogle() {
       // ...
     });
   };
-
+ function reggoogle(){ firebase.auth().getRedirectResult().then(function(result) {
+    if (result.credential) {
+      // This gives you a Google Access Token.
+      var token = result.credential.accessToken;
+    }
+    var user = result.user;
+  });
+  
+  // Start a sign in process for an unauthenticated user.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope('profile');
+  provider.addScope('email');
+  firebase.auth().signInWithRedirect(provider);
+};
 var app = new Vue({
     el: "#app",
     data: {
